ba7491fdc65294bf7c0377b424d736ec5b00d25d
b-luis <bscarletluis@gmail.com>
2023-11-24 12:34:13 +0800

    feat: App.jsx Initial routes and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,34 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Home, CreatePost } from "./pages";
+import { logo } from "./assets";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 p-4 border-b border-b-[#E6EBF4]">
+        <Link to="/">
+          <img
+            className="w-28 object-contain"
+            width={112}
+            src={logo}
+            alt="OpenAI logo"
+          />
+        </Link>
+        <Link
+          to="/create-post"
+          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;

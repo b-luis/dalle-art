c7ce7a42b1cc85984dd83fa5bb77a2e892831111
b-luis <bscarletluis@gmail.com>
2023-11-24 12:35:32 +0800

    feat: configurations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       screens: {

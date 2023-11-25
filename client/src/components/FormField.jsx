2a51a30a04ae93200e7622a3ba61652b13d36d98
b-luis <bscarletluis@gmail.com>
2023-11-25 13:40:10 +0800

    feat: form field component - added a reusable component for form field   * isSurpriseMe -> makes the component reusable, this prop is used to put a 'Surprise Me!' button                     next to label name.   * props        -> labelName - dynamic name for input fields                     type      - button type                     name      - for controlled input name                     placeholder - temp name / description                     value     - for controlled input value                     handleChange - function to put the changes in the declared states                     handleSurpriseMe - function that handles the fetching of random prompts which uses the                                        function from utils/index.js (getRandomPrompt)                     isSurpriseMe - prop only used to put the 'Surprise Me! button

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,5 +1,49 @@
-const FormField = () => {
-  return <div>FormField</div>;
+/* eslint-disable react/prop-types */
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {/* since this is a reusable form field, one way to distinguish it 
+            to use it in the prompt label, is to add the isSurpiseME boolean
+            if that exists in the given value, the Surprise Me! button will show
+        */}
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-md text-black hover:bg-[#6469FF] hover:text-white"
+          >
+            Surprise Me!
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469FF] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
+    </div>
+  );
 };
 
 export default FormField;
